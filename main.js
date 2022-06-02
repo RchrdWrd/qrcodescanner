f06178e4a4bd32baccb2146b6a59072be50596a4
RchrdWrd <103904534+RchrdWrd@users.noreply.github.com>
2022-06-02 12:51:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
   Function: It's just a little manager module to make coding easier with qr-scanner.min.js.
 */
 
-import QrScanner from "./modules/qr-scanner/qr-scanner.min.js";
+import QrScanner from "./modules/qr-scanner/qr-scanner.min.js"
 
 function QrCode(videoElementId, resultHandler, errorHandler) {
   const scanner = new QrScanner(document.getElementById(videoElementId), (result) => resultHandler(result), {
