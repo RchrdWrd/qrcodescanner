b5bc00ee09d5ef0b3458118d96db8502a4035aa9
RchrdWrd <richard.daily.ward@gmail.com>
2022-06-03 14:13:50 +0200

    updated

--- main.js
+++ main.js
@@ -7,6 +7,15 @@
 import QrScanner from "./modules/qr-scanner/qr-scanner.min.js"
 
 function QrCode(videoElementId, resultHandler, errorHandler) {
+  const cameras = []
+  var cameraIndex = 0
+
+  QrScanner.listCameras(true).then((camera) => {
+    camera.map((camera) => {
+      cameras.push({ CameraId: camera.id, CameraLabel: camera.label })
+    })
+  })
+
   const scanner = new QrScanner(
     document.getElementById(videoElementId),
     (result) => resultHandler(result),
@@ -17,12 +26,41 @@ function QrCode(videoElementId, resultHandler, errorHandler) {
       highlightScanRegion: true,
       highlightCodeOutline: true,
     }
-  );
+  )
+
+  function sleep(delay = 0) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, delay)
+    })
+  }
+
+  function hasMoreCamera() {
+    if (cameras.length > 1) return true
+    else return false
+  }
+
+  function switchCamera() {
+    if (cameraIndex === cameras.length - 1) cameraIndex = 0
+    else cameraIndex += 1
+    scanner.setCamera(cameras[cameraIndex].CameraId)
+  }
+
+  function start() {
+    scanner.setCamera(cameras[cameraIndex].CameraId)
+    scanner.start()
+  }
+
+  function stop() {
+    scanner.stop()
+  }
 
   return {
-    scanner: scanner,
-    QrScanner: QrScanner,
-  };
+    cameras: cameras,
+    hasMoreCamera: hasMoreCamera,
+    switchCamera: switchCamera,
+    start: start,
+    stop: stop,
+  }
 }
 
 export default QrCode
